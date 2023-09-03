80ee62eb589f1fd96ae2a5bdfcb86fc16ec0f957
rammeshulam <ram.meshulam@gmail.com>
2023-09-03 15:10:45 +0300

    fix promt

--- streamlit_chat1.py
+++ streamlit_chat1.py
@@ -11,7 +11,8 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-    openai.api_key = st.secrets["openai_api_key"]
+if prompt := st.chat_input():
+    openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
