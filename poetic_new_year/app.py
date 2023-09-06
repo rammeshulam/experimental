bb9939a71350ce9490e0fc2ce593967411ea9eb7
rammeshulam <ram.meshulam@gmail.com>
2023-09-06 23:53:00 +0300

    Add files via upload

--- poetic_new_year/app.py
+++ poetic_new_year/app.py
@@ -4,17 +4,21 @@ from PIL import Image, ImageDraw, ImageFont
 from pathlib import Path
 
 def create_image(image, size, font, message):
+    """Adds text to the center of a given image"""
+
     W, H = size
     draw = ImageDraw.Draw(image)
     _, _, w, h = draw.textbbox((0, 0), message, font=font)
     draw.text(((W-w)/2, (H-h)/2), message, fill='blue', font=font)
     return image
 
+#read the prompt
 path = Path(__file__).parents[0]
 context = ''
 with open(path/'prompt.txt') as f:
     context = f.read()
 
+#Draw the app and form
 st.title("Poetic Happy New Year")
 
 with st.form("greeting_input", clear_on_submit=False):
@@ -24,22 +28,25 @@ with st.form("greeting_input", clear_on_submit=False):
         placeholder="Recipient Name",
         label_visibility="collapsed"
     )
+
     st.markdown("4-5 things they love. They love... ")
     fun_facts = st.text_input(
         label="Complete the sentence: They love...",
         placeholder="develop code, go to the beach, \
-        pizza, their dog Jessica and reading books",
+pizza, their dog Jessica and reading books",
         label_visibility="collapsed"
     )
 
     st.form_submit_button("Send", use_container_width=True)
 
+#configure the model
 try:
     palm.configure(api_key=st.secrets.palm_api_key)
 except Exception as e:
     print(e)
     st.info("Please pass a valid API key")
 
+#given the inputs, generate a personal poem
 if (fun_facts and recipient):
     
     response = palm.generate_text(
@@ -47,8 +54,12 @@ if (fun_facts and recipient):
         temperature=0.05)
     
     poem = response.result.replace('\n','\n\n')
-
-    #font = ImageFont.truetype(str(path/"KirimomiSwaIt.ttf"), 35)
-    font = ImageFont.truetype("./KirimomiSwaIt.ttf", 35)
-    i = create_image(Image.open(str(path/'shana-tova-bg.jpeg')), (1024,1024), font, poem)
+    
+    # Add the generated poem to the background image
+    font_path = str(path/"KirimomiSwaIt.ttf")
+    font = ImageFont.truetype(font_path, 35)
+    
+    bg_path = str(path/'shana-tova-bg.jpeg')
+    i = create_image(Image.open(bg_path), (1024,1024), font, poem)
+    
     st.image(i)
